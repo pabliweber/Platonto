ce109f10e8f3e7eed1d893c7d77731af1bfdcc36
TomasVrbada <pabliweber@gmail.com>
2024-05-24 14:43:24 -0300

    Add files via upload

---
+++ Calculadora.js
@@ -0,0 +1,4 @@
+const txtop1 = document.getElementById("op1");
+const txtoperador = document.getElementById("operado");
+const txtop2 = document.getElementById("op");
+const b = document.getElementById("");
